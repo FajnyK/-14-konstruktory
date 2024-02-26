b9b2e4f16c80bd238081cc8026c23a5ff1cf5bf4
Ivan Keynes <fajny.kod.pl@gmail.com>
2024-02-26 16:16:59 +0100

    Initial commit

---
+++ Kot.cs
@@ -0,0 +1,14 @@
+﻿namespace _14_konstruktory
+{
+    public class Kot(string imie, int wiek)
+    {
+        public Kot(int wiek) : this("bella", wiek)
+        {
+
+        }
+        public void PokazInformacje()
+        {
+            Console.WriteLine($"imie: {imie} \nwiek: {wiek}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using _14_konstruktory;
+
+
+Kot bella = new Kot(5);
+
+bella.PokazInformacje();
+
+
+Kot mruczek = new Kot("Mruczek", 6);
+
+
+mruczek.PokazInformacje();
